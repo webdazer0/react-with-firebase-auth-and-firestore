16d62e2b14b9862e528b9b9a5605e0cb44c043f9
Miguel C <webdazer0@gmail.com>
2023-02-05 04:45:16 +0100

    chore: improve Navbar for user verified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,105 +1,110 @@
 import React from "react";
+import { REGISTER_WITH_EMAIL } from "../utilities/utils.email";
+import IconCheck from "./icons/IconCheck";
+import { NavLink } from "react-router-dom";
+import NavbarStyled from "./Navbar.styled";
 
-function Navbar({
-  setModal,
-  setRegister,
-  setLogin,
-  logout,
-  handleLogout,
-  profile,
-  verifyEmail,
-}) {
-  const handleModal = (e, myfunction) => {
+function Navbar({ user, logout, profile, verifyEmail }) {
+  const handleLogout = (e) => {
     e.preventDefault();
-    myfunction(true);
-    setModal(true);
+    logout();
   };
+
+  const MenuUserNotLogged = () => (
+    <NavigationMenu>
+      <MyNavLink to={"/login"}>Signin</MyNavLink>
+      <MyNavLink to={`/login/${REGISTER_WITH_EMAIL}`}>SignUp</MyNavLink>
+    </NavigationMenu>
+  );
+
+  const MenuUser = () => (
+    <NavigationMenu>
+      {profile && <MyAvatar profile={profile} verifyEmail={verifyEmail} />}
+      <MyLink onClick={handleLogout}>Logout</MyLink>
+    </NavigationMenu>
+  );
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <a className="navbar-brand" href="/">
-          <h1 className="h1">LinksApp</h1>
-        </a>
-        <button
-          className="navbar-toggler collapsed"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="navbar-collapse collapse" id="navbarNav">
-          <ul className="navbar-nav d-flex justify-content-between ml-auto">
-            {logout ? (
-              <>
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    href="/"
-                    onClick={(e) => handleModal(e, setLogin)}
-                  >
-                    Signin
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    href="/"
-                    onClick={(e) => handleModal(e, setRegister)}
-                  >
-                    SignUp
-                  </a>
-                </li>
-              </>
-            ) : (
-              <>
-                {console.log("profile")}
-                {console.log(profile)}
-                {profile && (
-                  <li className="nav-item d-flex justify-content-center align-items-center">
-                    <a
-                      className="h5 d-flex justify-content-center align-items-center m-0"
-                      href="/"
-                    >
-                      <img
-                        src={profile.photoURL}
-                        width="40"
-                        height="40"
-                        className="d-inline-block align-top pr-1"
-                        alt=""
-                      />
-                      {profile.displayName
-                        ? profile.displayName
-                        : profile.email}
-                    </a>
-                    {profile.emailVerified ? (
-                      <span className="text-success pl-1"> ✔</span>
-                    ) : (
-                      <button onClick={() => verifyEmail()}>
-                        Verificare Email
-                      </button>
-                    )}
-                  </li>
-                )}
-                <li className="nav-item">
-                  <a
-                    className="nav-link"
-                    href="/"
-                    onClick={(e) => handleLogout(e)}
-                  >
-                    Logout
-                  </a>
-                </li>
-              </>
-            )}
-          </ul>
+    <NavbarStyled>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            <h1 className="lead font-weight-bold mb-0">LinksApp</h1>
+          </a>
+          <button
+            className="navbar-toggler collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {user ? <MenuUser /> : <MenuUserNotLogged />}
         </div>
-      </div>
-    </nav>
+      </nav>
+    </NavbarStyled>
   );
 }
 
 export default Navbar;
+
+const MyNavLink = ({ to, children }) => (
+  <li className="nav-item">
+    <NavLink className="nav-link" to={to} alt="nav-link">
+      {children}
+    </NavLink>
+  </li>
+);
+
+const MyLink = ({ onClick, children }) => (
+  <li className="nav-item">
+    <a href="/" className="nav-link" onClick={onClick} alt="nav-link">
+      {children}
+    </a>
+  </li>
+);
+
+const NavigationMenu = ({ children }) => (
+  <div className="navbar-collapse collapse" id="navbarNav">
+    <ul className="navbar-nav d-flex justify-content-between ml-auto">
+      {children}
+    </ul>
+  </div>
+);
+
+const MyAvatar = ({ profile, verifyEmail }) => {
+  const { displayName, photoURL, email, emailVerified } = profile;
+
+  const displayNameText = displayName ?? email;
+
+  return (
+    <li className="navbar-avatar nav-item d-flex justify-content-center align-items-center">
+      <a
+        className="h5 d-flex justify-content-center align-items-center m-0 text-white"
+        href="/"
+      >
+        <MyAvatarImage src={photoURL} />
+        <span className="small">{displayNameText}</span>
+      </a>
+      {emailVerified ? (
+        <IconCheck />
+      ) : (
+        <button onClick={verifyEmail}>Verificare Email</button>
+      )}
+    </li>
+  );
+};
+
+const MyAvatarImage = ({ src, size }) => (
+  <img
+    src={src ?? "https://via.placeholder.com/40"}
+    width={size ?? "30"}
+    height={size ?? "30"}
+    className="bg-info rounded-circle d-inline-block align-top"
+    alt="profile avatar"
+    referrerPolicy="no-referrer"
+  />
+);
